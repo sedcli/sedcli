2f173a2d723e9be6222e19757c7ebe0909720b08
Andrzej Jakowski <andrzej.jakowski@intel.com>
2020-02-24 19:26:00 -0700

    sedcli-kmip: incorporating fixes after review
    This patch introduces sanity checks for metadata serializer preventing
    from situation when caller of the API invokes it for NULL pointer.
    
    Signed-off-by: Andrzej Jakowski <andrzej.jakowski@intel.com>

--- src/metadata_serializer.c
+++ src/metadata_serializer.c
@@ -33,87 +33,109 @@ void sedcli_metadata_free_buffer(struct sedcli_metadata *buffer)
 void sedcli_metadata_init(struct sedcli_metadata *meta, uint32_t pek_id_size,
 		uint32_t iv_size, uint32_t enc_dek_size, uint32_t tag_size)
 {
-	meta->magic_num = htole64(SEDCLI_META_MAGIC);
-	meta->version = htole32(SEDCLI_META_VERSION);
+	if (meta != NULL) {
+		meta->magic_num = htole64(SEDCLI_META_MAGIC);
+		meta->version = htole32(SEDCLI_META_VERSION);
 
-	meta->pek_id_size = htole32(pek_id_size);
-	meta->iv_size = htole32(iv_size);
-	meta->enc_dek_size = htole32(enc_dek_size);
-	meta->tag_size = htole32(tag_size);
+		meta->pek_id_size = htole32(pek_id_size);
+		meta->iv_size = htole32(iv_size);
+		meta->enc_dek_size = htole32(enc_dek_size);
+		meta->tag_size = htole32(tag_size);
+	}
 }
 
 uint8_t *sedcli_meta_get_pek_id_addr(struct sedcli_metadata *meta)
 {
-	return meta->data;
+	return meta == NULL ? NULL : meta->data;
 }
 
 uint8_t *sedcli_meta_get_iv_addr(struct sedcli_metadata *meta)
 {
 	uint32_t offset = 0;
 
-	offset += sedcli_meta_get_pek_id_size(meta);
+	if (meta != NULL) {
+		offset += sedcli_meta_get_pek_id_size(meta);
 
-	return &meta->data[offset];
+		return &meta->data[offset];
+	}
+
+	return NULL;
 }
 
 uint8_t *sedcli_meta_get_enc_dek_addr(struct sedcli_metadata *meta)
 {
 	uint32_t offset = 0;
 
-	offset += sedcli_meta_get_pek_id_size(meta);
-	offset += sedcli_meta_get_iv_size(meta);
+	if (meta != NULL) {
+		offset += sedcli_meta_get_pek_id_size(meta);
+		offset += sedcli_meta_get_iv_size(meta);
 
-	return &meta->data[offset];
+		return &meta->data[offset];
+	}
+
+	return NULL;
 }
 
 uint8_t *sedcli_meta_get_tag_addr(struct sedcli_metadata *meta)
 {
 	uint32_t offset = 0;
 
-	offset += sedcli_meta_get_pek_id_size(meta);
-	offset += sedcli_meta_get_iv_size(meta);
-	offset += sedcli_meta_get_enc_dek_size(meta);
+	if (meta != NULL) {
+		offset += sedcli_meta_get_pek_id_size(meta);
+		offset += sedcli_meta_get_iv_size(meta);
+		offset += sedcli_meta_get_enc_dek_size(meta);
 
-	return &meta->data[offset];
+		return &meta->data[offset];
+	}
+
+	return NULL;
 }
 
 uint32_t sedcli_meta_get_pek_id_size(struct sedcli_metadata *meta)
 {
-	return le32toh(meta->pek_id_size);
+	return meta != NULL ? le32toh(meta->pek_id_size) : 0;
 }
 
 void sedcli_meta_set_pek_id_size(struct sedcli_metadata *meta, uint32_t pek_id_size)
 {
-	meta->pek_id_size = htole32(pek_id_size);
+	if (meta != NULL) {
+		meta->pek_id_size = htole32(pek_id_size);
+	}
 }
 
 uint32_t sedcli_meta_get_iv_size(struct sedcli_metadata *meta)
 {
-	return le32toh(meta->iv_size);
+	return meta != NULL ? le32toh(meta->iv_size) : 0;
 }
 
 void sedcli_meta_set_iv_size(struct sedcli_metadata *meta, uint32_t iv_size)
 {
-	meta->iv_size = htole32(iv_size);
+	if (meta != NULL) {
+		meta->iv_size = htole32(iv_size);
+	}
 }
 
 uint32_t sedcli_meta_get_enc_dek_size(struct sedcli_metadata *meta)
 {
-	return le32toh(meta->enc_dek_size);
+	return meta != NULL ? le32toh(meta->enc_dek_size) : 0;
 }
 
 void sedcli_meta_set_enc_dek_size(struct sedcli_metadata *meta, uint32_t enc_dek_size)
 {
-	meta->enc_dek_size = htole32(enc_dek_size);
+	if (meta != NULL) {
+		meta->enc_dek_size = htole32(enc_dek_size);
+	}
 }
 
 uint32_t sedcli_meta_get_tag_size(struct sedcli_metadata *meta)
 {
-	return le32toh(meta->tag_size);
+	return meta != NULL ? le32toh(meta->tag_size) : 0;
 }
 
 void sedcli_meta_set_tag_size(struct sedcli_metadata *meta, uint32_t tag_size)
 {
-	meta->tag_size = htole32(tag_size);
+	if (meta != NULL) {
+		meta->tag_size = htole32(tag_size);
+	}
 }
 
