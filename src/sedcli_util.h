6c71667888642593ecca7f9d5c32835585e02a3d
Andrzej Jakowski <andrzej.jakowski@intel.com>
2020-03-10 13:22:44 -0700

    sedcli: move get_lock_type() function to utility file
    This patch moves get_lock_type() function to utility file allowing it
    to be reused by other interface (e.g. sedcli-kmip).
    
    Signed-off-by: Andrzej Jakowski <andrzej.jakowski@intel.com>

--- src/sedcli_util.h
+++ src/sedcli_util.h
@@ -6,6 +6,8 @@
 #ifndef _SEDCLI_UTIL_H_
 #define _SEDCLI_UTIL_H_
 
+int get_lock_type(const char *lock_type);
+
 int get_password(char *pwd, uint8_t *len, uint8_t min, uint8_t max);
 
 #endif /* _SEDCLI_UTIL_H_ */
